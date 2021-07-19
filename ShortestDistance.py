7524a265224bf8f977f44022a0a672dcc5d134e1
Satyam Shree <satyamshree780@gmail.com>
2021-07-19 21:49:19 +0530

    Accepted

--- ShortestDistance.py
+++ ShortestDistance.py
@@ -1,37 +1,36 @@
 from collections import defaultdict,deque
 import math
+import heapq as hq
 import sys
-sys.setrecursionlimit(1000000)
-def dfs(graph,lis,s,visited):
-    visited[s]=True
-    for i in graph[s]:
-        if not visited:
-            dfs(graph,lis,i,visited)
-    lis.append(s)        
 
-
+""" 
+    dijkstra shortest path
+"""
 
 if __name__ == "__main__":
     n,m = map(int,input().split())
-    d = defaultdict(lambda:math.inf)
-    graph = defaultdict(list)
+    d = defaultdict(list)
     visited = [False]*(n+1)
     for i in range(m):    
         a,b,c = map(int,input().split())
-        d[(a,b)] = min(d[(a,b)],c)
-        graph[a].append(b)
+        d[a].append((b,c))
+       
         
     dis = [math.inf]*(n+1) 
 
-    lis = []
-    for i in range(1,n+1):
-        if not visited[i]:
-            dfs(graph,lis,i,visited)
-
-    print(lis)
     dis[1] = 0
-    for i in lis:
-        for j in graph[i]:
-            if dis[j] > dis[i] + d[(i,j)]:
-                dis[j] = dis[i] + d[(i,j)]
-    print(*dis[1:])            
+
+    heap = []
+    hq.heappush(heap,(0,1))
+    while heap:
+        x = hq.heappop(heap)
+        if not visited[x[1]]:
+            visited[x[1]] = True
+            for i in d[x[1]]:
+                if dis[i[0]] > dis[x[1]]+i[1]:
+                    dis[i[0]] = dis[x[1]]+i[1]
+                    hq.heappush(heap,(dis[i[0]],i[0]))
+    print(*dis[1:])                
+
+
+        
