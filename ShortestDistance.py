65ffde47b7f58fa53b7ba760b005964bd59bc650
Satyam Shree <satyamshree780@gmail.com>
2021-07-21 22:57:22 +0530

    failed attempt at shortest path 2

--- ShortestDistance.py
+++ ShortestDistance.py
@@ -1,36 +1,18 @@
-from collections import defaultdict,deque
-import math
-import heapq as hq
-import sys
-
-""" 
-    dijkstra shortest path
-"""
-
+from collections import defaultdict as u;import math;import heapq as e
 if __name__ == "__main__":
-    n,m = map(int,input().split())
-    d = defaultdict(list)
-    visited = [False]*(n+1)
-    for i in range(m):    
-        a,b,c = map(int,input().split())
-        d[a].append((b,c))
-       
-        
-    dis = [math.inf]*(n+1) 
-
-    dis[1] = 0
-
-    heap = []
-    hq.heappush(heap,(0,1))
-    while heap:
-        x = hq.heappop(heap)
-        if not visited[x[1]]:
-            visited[x[1]] = True
-            for i in d[x[1]]:
-                if dis[i[0]] > dis[x[1]]+i[1]:
-                    dis[i[0]] = dis[x[1]]+i[1]
-                    hq.heappush(heap,(dis[i[0]],i[0]))
-    print(*dis[1:])                
+ n,m=map(int,input().split());d=u(list);v=[0]*(n+1)
+ for i in range(m):    
+  a,b,c=map(int,input().split());d[a].append((b,c))
+ p=[math.inf]*(n+1);p[1]=0;h=[]
+ e.heappush(h,(0,1))
+ while h:
+  s,r=e.heappop(h)
+  if not v[r]:
+   v[r]=1
+   for i,y in d[r]:
+    if p[i]>p[r]+y:
+     p[i]=p[r]+y;e.heappush(h,(p[i],i))
+ print(*p[1:])                
 
 
         
